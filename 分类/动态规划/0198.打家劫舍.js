899aba458f77f53641ec143480fc0a3bcb99a5fd
nzh233 <1083225709@qq.com>
2020-10-30 16:51:26 +0800

    add new solution

--- 分类/动态规划/0198.打家劫舍.js
+++ 分类/动态规划/0198.打家劫舍.js
@@ -11,9 +11,25 @@ var rob = function(nums) {
         if (nums.length > 2) {
             dp[2] = nums[0] + nums[2];
             for (let i = 3; i < dp.length; i++) {
-                dp[i] = Math.max(dp[i-2], dp[i-3]) + nums[i];
+                dp[i] = Math.max(dp[i-2], dp[i-3]) + nums[i]; // 抢第i家之前，抢第i-2家或抢第i-3家
             }
         }
     }
     return Math.max(...dp);
 };
+
+// 另一思路，比较抢了i-1家与抢i-2家和i家的大小
+// dp[i] = Math.max(dp[i - 2] + nums[i - 2], dp[i - 1]);
+// 空间优化后
+var rob = function(nums) {
+    let a = 0;
+    let b = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+    const temp = b;
+    b = Math.max(a + nums[i], b);
+    a = temp;
+    }
+
+    return b;
+}
